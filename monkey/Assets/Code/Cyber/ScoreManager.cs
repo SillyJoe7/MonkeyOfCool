345dc5a203cf944fa1eea9c75496c64f672241d0
SillyJoe7 <beau.bockus@gmail.com>
2025-11-07 11:11:41 -0600

    Created Points

---
+++ monkey/Assets/Code/Cyber/ScoreManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI; // only if you’re displaying score in UI
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager instance; // allows global access
+    public int score = 0;
+    public Text scoreText; // optional, if using UI Text
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    public void AddPoint()
+    {
+        score++;
+        Debug.Log("Score: " + score);
+
+        if (scoreText != null)
+            scoreText.text = "Score: " + score;
+    }
+}
